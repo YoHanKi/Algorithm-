58161ea4b7be129250108b343a09738138c4b4e3
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-08-09 01:00:29 +0900

    최대값을 구하는 알고리즘
    최대값을 구하는 알고리즘

---
+++ Max3.java
@@ -0,0 +1,16 @@
+package chap01;
+
+public class Max3 {
+	//abc의 최댓값을 구하여 반환
+	static int max3(int a, int b, int c) {
+		int max = a;
+		if(b > max) max = b;
+		if(c > max) max = c;
+		
+		return max;
+	}
+	
+public static void main(String [] args) {
+	System.out.println("max3(3,2,1)= " + max3(3,2,1));
+}
+}
