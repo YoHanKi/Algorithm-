9d8ca7d873a06f9ec9c23616df3d0039dcddd99e
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-08-27 00:03:33 +0900

    버블 정렬(3/3 ver)
    버블 정렬(3/3 ver)

---
+++ BubbleSort3.java
@@ -0,0 +1,52 @@
+package chap06;
+
+import java.util.Scanner;
+/*버블 정렬
+버블 정렬이란 기포가 마치 아래에서 위로 올라오듯 정렬을 한 방향에서 비교하며 이동하여 정렬하는 방식*/
+// 특정 시점에서 교환이 이루어지지않는다면, 특정시점 이후는 정렬이 완료되었기 때문에 제외
+public class BubbleSort3 {
+ 
+	// a[idx1]과 a[idx2]의 값을 교환
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	//버블 정렬
+	static void bubbleSort(int[] a, int n) {
+		int k = 0;
+		while (k < n-1) {
+			int last = n-1;
+			for (int j = n -1; j>k ; j--)
+				if(a[j - 1] > a[j]) {
+					swap(a,j - 1, j);
+					last = j;
+				}
+			k = last;
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("버블정렬(ver.3)");
+		System.out.print("요솟수 : ");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+		
+		for (int i = 0; i < nx; i++) {
+			System.out.print("x["+i+"]: ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		bubbleSort(x, nx);
+		
+		System.out.println("오름차순으로 정렬");
+		for(int i = 0; i < nx; i++)
+			System.out.println("x[" + i + "]=" + x[i]);
+	}
+
+}
